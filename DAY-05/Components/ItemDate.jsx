3c6789d158399fbaea9370a910203ccece5c2270
RahulJingar <rjingarnsd@gmail.com>
2025-04-08 23:59:36 +0530

    first commit

---
+++ DAY-05/Components/ItemDate.jsx
@@ -0,0 +1,11 @@
+function ItemDate(){
+    const day = 27;
+    const month = september;
+    const year = 1998;
+
+    return (<div>
+        <span>{day}</span>
+        <span>{month}</span>
+        <span>{year}</span>
+    </div>)
+}
---
+++ DAY-05/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ItemDate from '../../DAY-04/Components/ItemDate'
+
+function App() {
+
+  return (
+    <>
+    <ItemDate/>
+     <h1>Hello Guys</h1>
+    </>
+  )
+}
+
+export default App
