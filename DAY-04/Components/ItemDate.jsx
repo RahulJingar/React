495176ce6e3cc67e8c1316739252cc7ab43167f6
RahulJingar <rjingarnsd@gmail.com>
2025-04-08 23:51:21 +0530

    first commit

---
+++ DAY-04/Components/ItemDate.jsx
@@ -0,0 +1,11 @@
+import './ItemDate.css'
+
+function ItemDate(){
+    return (<div>
+        <div>27</div>
+        <div>sept</div>
+        <div>1998</div>
+    </div>);
+}
+
+export default ItemDate;
---
+++ DAY-04/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ItemDate from '../Components/ItemDate'
+
+function App() {
+
+  return (
+    <>
+      <ItemDate/>
+      <div>Hello World</div>
+    </>
+  )
+}
+
+export default App
